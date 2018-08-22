a57e1277b290223abed90894472c095e226d66f8
Andrew Sprague <andrewsprague@outlook.com>
2018-08-22 23:00:42 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,9 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |func, lang|
+    lang.each do |name, type|
+      new_hash[name] = lang.to_s
+    end
+  end
 end
